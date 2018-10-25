ff8acfc2f6fe0dda2adfd20d09d0d72bf7d8bff5
Thaísa Borsato Lopes <36734142+thaisaborsato@users.noreply.github.com>
2018-10-25 08:39:12 -0300

    Adicionados arquivos do projeto.

---
+++ src/main/java/br/inatel/ec206/integracaocontinua/IntegracaoContinua.java
@@ -0,0 +1,9 @@
+package br.inatel.ec206.integracaocontinua;
+
+public class IntegracaoContinua {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+}
